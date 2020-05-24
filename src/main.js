353c11532a9c36d9836cb2a012024f66432c779b
lingxuan588 <1096121491@qq.com>
2020-05-24 11:40:03 +0800

    注册使用vant完成

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'// 完整引入vant
+import 'vant/lib/index.css'// 引入vant组件的样式
+Vue.use(Vant)// 注册使用
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/video/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>视频模块页面</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
