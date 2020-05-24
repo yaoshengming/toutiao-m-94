74f59cdb8dcdb7ae1f1726382916b3c9fba6d929
lingxuan588 <1096121491@qq.com>
2020-05-24 23:12:19 +0800

    登录-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,26 @@
 <template>
-<div>
+  <div>
     <div class="container">
-      <van-nav-bar title="登录" left-text="返回"     @click-left="$router.go(-1)"></van-nav-bar>
+      <van-nav-bar title="登录" left-text="返回" @click-left="$router.go(-1)"></van-nav-bar>
+      <van-cell-group>
+        <van-field label="手机号" placeholder="请输入手机号" ></van-field>
+        <van-field label="验证码" placeholder="请输入验证码" >
+        <van-button slot="button"   size="small" type="primary">发送验证码</van-button>
+        </van-field>
+         <div class='login-box'>
+      <van-button type="info" round size="small" block>登录</van-button>
+    </div>
+      </van-cell-group>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
+.login-box {
+  padding: 20px
+}
 </style>
