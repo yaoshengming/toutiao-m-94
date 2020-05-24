d9197ee8394fc5b98133bb1f47fe4c23aa137030
lingxuan588 <1096121491@qq.com>
2020-05-24 13:00:58 +0800

    完成路由规则配置

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import Vant from 'vant'// 完整引入vant
 import 'vant/lib/index.css'// 引入vant组件的样式
+import 'amfe-flexible'
 Vue.use(Vant)// 注册使用
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home'
 
 Vue.use(VueRouter)
-
+// 按需加载配置路由
+const Layout = () => import('@/views/layout')// 按需引入layout
+const home = () => import('@/views/home')// 按需引入home 二级路由
+const question = () => import('@/views/question')// 按需引入question 二级路由
+const video = () => import('@/views/video')// 按需引入video 二级路由
+const user = () => import('@/views/user')// 按需引入user 二级路由
+// 其他一级路由
+const chat = () => import('@views/user/chat')// 小智同学
+const login = () => import('@views/user/login')// 登录
+const profile = () => import('@views/user/profile')// 编辑资料
+const search = () => import('@views/user/search')// 搜索中心
+const searchresult = () => import('@views/user/result')// 搜索结果
+const article = () => import('@views/user/article')// 文章详情
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Layout, // 布局组件 一级路由
+    children: [{
+      path: '',
+      component: home
+    }, {
+      path: '/question',
+      component: question// 问答组件
+    }, {
+      path: '/video',
+      component: video// 视频组件
+    }, {
+      path: '/user',
+      component: user
+    }]
+  }, {
+    path: '/user/chat',
+    component: chat// 一级路由小智同学
+  },
+  {
+    path: '/login',
+    component: login // 一级路由 登录
+  },
+  {
+    path: '/user/profile',
+    component: profile// 一级路由编辑资料
+  },
+  {
+    path: '/search',
+    component: search// 一级路由 搜索中心
+  },
+  {
+    path: '/search/result',
+    component: searchresult // 一级路由搜索中心
+  },
+  {
+    path: '/user/article',
+    component: article // 一级路由 文章详情
   }
-
 ]
 
 const router = new VueRouter({
