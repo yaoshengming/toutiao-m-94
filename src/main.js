7a1938148dd0f868f807aee36f39d9d728f3fe28
lingxuan588 <1096121491@qq.com>
2020-05-25 16:19:36 +0800

    修改vant主题蓝色

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'// 完整引入vant
-import 'vant/lib/index.css'// 引入vant组件的样式
+import 'vant/lib/index.less'// 引入vant组件的样式 这里用less 因为我们想修改某些变量
 import 'amfe-flexible'
 import '@/styles/index.less'// 引入全局的自定义样式  因为要覆盖vant的样式
 Vue.use(Vant)// 注册使用
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// 需要导出一个对象
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          blue: '#3296fa'
+        }
+      }
+    }
+  }
+}
