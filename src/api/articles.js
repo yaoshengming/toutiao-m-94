161866cf23ec29d49635ff7a36e131226153fa4c
lingxuan588 <1096121491@qq.com>
2020-05-26 18:56:16 +0800

    文章列表-封装渲染列表方法

---
+++ src/api/articles.js
@@ -0,0 +1,9 @@
+// 专门处理文章模块的请求列表
+import request from '@/utils/articles'
+// 获取文章推荐数据
+export function getArticles (params) {
+  return request({
+    url: 'http://ttapi.research.itcast.cn/app/v1_1/articles',
+    params: { with_top: 1, ...params }
+  })
+}
