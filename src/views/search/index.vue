ae383aa588c2b2fda96a2b0327acda61605e4189
lingxuan588 <1096121491@qq.com>
2020-05-28 20:22:40 +0800

    搜索关键词 历史记录及联想

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -3,13 +3,16 @@
     <!-- 搜索组件一级路由   返回上一个页面-->
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
-    <van-search  placeholder="请输入搜索关键词" shape="round" />
-    <van-cell-group class="suggest-box" >
+    <!-- 搜索组件 -->
+    <van-search v-model.trim="q"    placeholder="请输入搜索关键词" shape="round" />
+    <!-- 联想内容 -->
+    <van-cell-group class="suggest-box" v-if="q">
       <van-cell icon="search">
         <span>j</span>ava
       </van-cell>
     </van-cell-group>
-    <div class="history-box">
+    <!-- 历史记录部分 你搜索的内容 会在这里记录 -->
+    <div class="history-box" v-else>
       <div class="head">
         <span>历史记录</span>
         <van-icon name="delete"></van-icon>
@@ -26,7 +29,12 @@
 
 <script>
 export default {
-  name: 'search'
+  name: 'search',
+  data () {
+    return {
+      q: ''// 关键词的
+    }
+  }
 }
 </script>
 
